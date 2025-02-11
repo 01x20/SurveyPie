c4e55bb13380a3410ce3768e350ede555a3c798b
kimgaeul <ge950120@gmail.com>
2025-02-11 11:48:09 +0900

    styled component 스타일 변수
    <Button /> styled component 생성 및 constants > color.js에서 변수로 동적 스타일 변경 실습

--- survey-pie/src/pages/SurveyPage/index.js
+++ survey-pie/src/pages/SurveyPage/index.js
@@ -6,21 +6,33 @@ import QuestionBox from '../../components/QuestionBox';
 
 function SurveyPage() {
   const params = useParams();
-  console.log(params);
   const questions = [
     {
       title: '질문1 입니다.',
       desc: '설명1 입니다',
       type: 'text',
       required: false,
-      options: {},
+      options: {
+        placeholder: 'placeholder 입니다.',
+      },
     },
     {
       title: '질문2 입니다.',
       desc: '설명2 입니다',
-      type: 'text',
+      type: 'textarea',
       required: false,
-      options: {},
+      options: {
+        placeholder: 'placeholder 입니다.',
+      },
+    },
+    {
+      title: '질문3 입니다.',
+      desc: '설명3 입니다',
+      type: 'select',
+      required: false,
+      options: {
+        items: ['답변1', '답변2', '답변3', '답변4', '답변5'],
+      },
     },
   ];
 
