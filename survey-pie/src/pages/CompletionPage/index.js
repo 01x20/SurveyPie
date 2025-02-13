716394e6dde87bc3a671735bb7ad5adc8b03424b
kimgaeul <ge950120@gmail.com>
2025-02-13 16:43:18 +0900

    Axios 설치 및 서버 세팅, 테스트

--- survey-pie/src/pages/CompletionPage/index.js
+++ survey-pie/src/pages/CompletionPage/index.js
@@ -1,6 +1,11 @@
+import axios from 'axios';
 import styled from 'styled-components';
 
 function CompletionPage() {
+  axios.get('http://localhost:3001/surveys').then((res) => {
+    console.log(res.data);
+  });
+
   return (
     <>
       <CompletionPageWrapper>CompletionPage</CompletionPageWrapper>
