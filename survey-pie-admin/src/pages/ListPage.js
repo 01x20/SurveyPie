7df32fb1b607d298ffff9f9fbc199ac4f8f476e8
kimgaeul <ge950120@gmail.com>
2025-02-19 16:06:00 +0900

    Survey-pie Admin 세팅 및 UI 라이브러리, useSWR 학습

---
+++ survey-pie-admin/src/pages/ListPage.js
@@ -0,0 +1,18 @@
+import useSWR from 'swr';
+
+import MainLayout from '../layouts/MainLayout';
+import fetcher from '../lib/fetcher';
+
+function ListPage() {
+  const { data, error } = useSWR('/surveys', fetcher);
+
+  console.log(data);
+
+  if (error) {
+    return 'error';
+  }
+
+  return <MainLayout selectedKeys={['list']}>ListPage</MainLayout>;
+}
+
+export default ListPage;
