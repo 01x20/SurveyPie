12ddbbe3dc85b6951db9c81a518c116c38c82c95
kimgaeul <ge950120@gmail.com>
2025-02-21 12:23:23 +0900

    질문 추가, 삭제, 이동 기능 구현
    - 각 요소의 컴포넌트를 생성
    - antd 사용법을 학습
    - immer api 사용법 학습 (데이터 불변성 유지)

--- survey-pie-admin/src/components/Card/index.js
+++ survey-pie-admin/src/components/Card/index.js
@@ -1,6 +1,15 @@
+import { DeleteOutlined, DownOutlined, UpOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 import styled from 'styled-components';
 
-function Card({ title, desc, children }) {
+function Card({
+  title,
+  desc,
+  children,
+  onUpButtonClick,
+  onDownButtonClick,
+  onDeleteButtonClick,
+}) {
   return (
     <CardWrapper>
       <Head>
@@ -8,15 +17,48 @@ function Card({ title, desc, children }) {
         <Desc>{desc}</Desc>
       </Head>
       <Body>{children}</Body>
+      <ButtonGroupWrapper>
+        <ButtonGroup>
+          <Button onClick={onUpButtonClick} type="text" icon={<UpOutlined />} />
+          <Button
+            onClick={onDeleteButtonClick}
+            type="text"
+            icon={<DeleteOutlined />}
+          />
+          <Button
+            onClick={onDownButtonClick}
+            type="text"
+            icon={<DownOutlined />}
+          />
+        </ButtonGroup>
+      </ButtonGroupWrapper>
     </CardWrapper>
   );
 }
+
+const ButtonGroupWrapper = styled.div`
+  position: absolute;
+  left: 100%;
+  top: 0;
+  display: none;
+`;
+const ButtonGroup = styled.div`
+  background: #fff;
+  margin-left: 10px;
+  border: 1px solid #dddd;
+  border-radius: 5px;
+`;
 
 const CardWrapper = styled.div`
   border: 1px solid #ddd;
   width: 400px;
   margin: 30px auto;
   background: #fff;
+  position: relative;
+
+  &:hover ${ButtonGroupWrapper} {
+    display: block;
+  }
 `;
 
 const Head = styled.div`
@@ -33,5 +75,4 @@ const Desc = styled.div`
 const Body = styled.div`
   padding: 15px;
 `;
-
 export default Card;
--- survey-pie-admin/src/components/PreviewSection/index.js
+++ survey-pie-admin/src/components/PreviewSection/index.js
@@ -1,13 +1,29 @@
+import AddButton from '../AddButton';
+import Body from '../Body';
 import Card from '../Card';
 
-function PreviewSection({ questions }) {
+function PreviewSection({
+  questions,
+  addQuestion,
+  moveUpQuestion,
+  moveDownQuestion,
+  deleteQuestion,
+}) {
   return (
     <>
-      {questions.map((questions, index) => (
-        <Card key={index} title={questions.title} desc={questions.desc}>
-          body
+      {questions.map((question, index) => (
+        <Card
+          key={index}
+          title={question.title}
+          desc={question.desc}
+          onUpButtonClick={() => moveUpQuestion(index)}
+          onDownButtonClick={() => moveDownQuestion(index)}
+          onDeleteButtonClick={() => deleteQuestion(index)}
+        >
+          <Body type={question.type} options={question.options} />
         </Card>
       ))}
+      <AddButton onClick={addQuestion} />
     </>
   );
 }
