198047350a85d93e5efe8d8c0f33189773a76fde
kimgaeul <ge950120@gmail.com>
2025-02-21 14:41:55 +0900

    Redux로 데이터 관리
    - Redux ToolKit을 활용하여 데이터 관리하는 법 학습

--- survey-pie-admin/src/components/AddButton/index.js
+++ survey-pie-admin/src/components/AddButton/index.js
@@ -1,12 +1,65 @@
+import '@ant-design/v5-patch-for-react-19';
+
 import { PlusCircleOutlined } from '@ant-design/icons';
+import { Button, Popover } from 'antd';
+import { useState } from 'react';
 import styled from 'styled-components';
 
-function AddButton({ onClick }) {
+function AddButton({ addQuestion }) {
+  const [open, setOpen] = useState(false);
+  const hide = () => {
+    setOpen(false);
+  };
+  const handleOpenChange = (newOpen) => {
+    setOpen(newOpen);
+  };
+
   return (
     <AddButtonWrapper>
-      <IconButton onClick={onClick}>
-        <PlusCircleOutlined />
-      </IconButton>
+      <Popover
+        content={
+          <div>
+            <Button
+              type="text"
+              onClick={() => {
+                hide();
+                addQuestion('select');
+              }}
+              style={{ display: 'block' }}
+            >
+              객관식
+            </Button>
+            <Button
+              type="text"
+              onClick={() => {
+                hide();
+                addQuestion('text');
+              }}
+              style={{ display: 'block' }}
+            >
+              단답식
+            </Button>
+            <Button
+              type="text"
+              onClick={() => {
+                hide();
+                addQuestion('textarea');
+              }}
+              style={{ display: 'block' }}
+            >
+              서술식
+            </Button>
+          </div>
+        }
+        trigger="click"
+        open={open}
+        placement="right"
+        onOpenChange={handleOpenChange}
+      >
+        <IconButton>
+          <PlusCircleOutlined />
+        </IconButton>
+      </Popover>
     </AddButtonWrapper>
   );
 }
